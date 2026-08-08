bb61f9fb9105cffbece750064c28c91a0c927616
sandhya kuppili <sandhyakuppili.84@gmail.com>
2026-08-08 21:08:28 +0530

    authentication applied for route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,8 +4,17 @@ const {
     login
 } = require("../controllers/authController");
 
+const authenticate = require("../middleware/authenticate");
+
 const router = express.Router();
 
 router.post("/login", login);
+
+router.get("/profile", authenticate, (req, res) => {
+    res.status(200).json({
+        message: "Protected route accessed",
+        user: req.user
+    });
+});
 
 module.exports = router;
